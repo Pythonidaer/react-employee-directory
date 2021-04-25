01c7b2f3d4e89813269ee81bd400388735e30d04
Pythonidaer <pythonidaer@gmail.com>
2021-04-25 14:15:13 -0400

    Settin up test documents and will begin coding the reac employee directory later. NOTE: the src must be renamed and a new one built in its place. At least now though the react app is tested and verified to work properly. I have added the homework README so that I can take notes on the expected outcome before leaving today.

---
+++ src/utils/API.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
+export default {
+  getRandomDog: function() {
+    return axios.get("https://dog.ceo/api/breeds/image/random");
+  },
+  getDogsOfBreed: function(breed) {
+    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
+  },
+  getBaseBreedsList: function() {
+    return axios.get("https://dog.ceo/api/breeds/list");
+  }
+};
